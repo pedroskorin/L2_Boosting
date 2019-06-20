0826646c091241e4847e3763cdd9b6dbab596369
pedroskorin <pedro.skorin@hotmail.com>
2019-06-19 23:25:59 -0300

    added_csv
    daded_csv

--- l2_boosting/main.R
+++ l2_boosting/main.R
@@ -2,6 +2,15 @@
 #
 #Implementation of the L2 Boosting algorithm
 #
-#theoretical source: Jing Zang, 
+#theoretical source: "Jing Zang, 2014, Forecasting Aggregates with Disaggregate Variables: 
+#Does boosting help to select the most informative predictors?"
 #
+#Authors: guislinden, pedroskorin
+#UFRGS
 #
+#2019-6-19
+
+data = read.csv("dados.csv")
+
+
+
